a1385d3066c0d9f05fe8b9ec42a9287a6eb7cc4b
adrian <adry.dabp@gmail.com>
2020-09-30 14:33:04 -0400

    voces fortaleza

--- src/components/voces-fortaleza/index.vue
+++ src/components/voces-fortaleza/index.vue
@@ -1,69 +1,80 @@
 <template>
-  <div class="app-quienes-somos">
-    <div class="app-quienes-somos__background"></div>
-    <div class="app-quienes-somos__container">
-      <v-container fluid>
-        Somos un Movimiento Ciudadano conformado por bolivianos que buscan manifestar amor al prójimo apoyando a través de distintas acciones a los más afectados de la crisis en la sociedad boliviana. No representamos a ningún partido político, institución o iglesia.
-        <br />
-        <br />
-        <div class="primary--text">VISIÓN</div>
-Ser un instrumento ciudadano sin fines lucrativos ni de interés político, conformado por voluntari@s con fe en Dios, cuyo objetivo es traer luz y esperanza en medio de la oscuridad, impactando positivamente a través de acciones que aportan al desarrollo y bien estar de la sociedad boliviana.
-        <br />
-        <br />
-        <div class="primary--text">MISIÓN</div>Ser un canalizador de ayuda para las personas que conforman la primera línea en la lucha contra la pandemia COVID-19, llevando material de bioseguridad, palabras de aliento, apoyo y esperanza.
-        Establecer un Centro de llamadas gratuito que atienda de forma continua a personas necesitadas de escucha empática y contención, para brindarles una voz de amor, aliento y apoyo.
-      </v-container>
-    </div>
-  </div>
+  <v-carousel
+    height="calc(100vh - 80px)"
+    hide-delimiter-background
+    show-arrows-on-hover
+    hide-delimiters
+    >
+    <v-carousel-item
+      v-for="(item,i) in aVoces"
+      :key="i"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+      <v-img
+        v-if="item.src"
+        :src="item.src"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+      <video v-else width="100%" class="video--voces" controls>
+        <source :src="item.video" type="video/mp4">
+      </video>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      show: false,
+      show: [],
       user: null,
       password: null,
+      aVoces: []
     };
   },
+  mounted () {
+    this.aVoces = [
+      {
+        title: 'La calidad de lo que consumimos se refleja en nuestra salud',
+        video: 'http://www.vocesfortaleza.com/assets/callCenter/WhatsApp%20Video%202020-08-30%20at%207.33.01%20PM.mp4',
+        descripcion: 'Im a thing. But, like most politicians, he promised more than he could deliver. '
+      },
+      {
+        title: 'La calidad de lo que consumimos se refleja en nuestra salud',
+        src: 'http://www.vocesfortaleza.com/assets/callCenter/arte%202.png',
+        descripcion: 'La calidad de lo que consumimos se refleja en nuestra salud, en nuestro cuerpo. Lo mismo sucede en el interior, la elección que tomamos para nutrir nuestro espíritu es un alimento que se ve reflejado en las acciones cotidianas. Asegúrate de buscar luz, recuerda que lo que consumas será lo que tendrás para ofrecer a los demás, nadie puede dar lo que no tiene. El amor nutre, servir a otros trae significado de vida, tener fe, elegir con prudencia nuestras acciones, pensamientos y nutrir nuestro espíritu buscando palabras de sabiduría, trae bendición. Muchas puertas esperan ser abiertas por tus manos para derramar sobre ti abundancia y felicidad, si quieres encontrar la llave, asegúrate de elegir correctamente el alimento de tu espíritu, esa elección abrirá tus ojos para mostrarte un camino.'
+      },
+      {
+        title: 'La calidad de lo que consumimos se refleja en nuestra salud',
+        src: 'http://www.vocesfortaleza.com/assets/callCenter/arte%204.png',
+        descripcion: 'Im a thing. But, like most politicians, he promised more than he could deliver. '
+      },
+      {
+        title: 'La calidad de lo que consumimos se refleja en nuestra salud',
+        src: 'http://www.vocesfortaleza.com/assets/callCenter/arte%205.png',
+        descripcion: 'Im a thing. But, like most politicians, he promised more than he could deliver. '
+      }
+    ];
+  }
 };
 </script>
 
 <style lang="scss">
 @import "../../scss/variables.scss";
-
-.app-quienes-somos {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: calc(100vh - 150px);
-  margin: auto;
-  text-align: center;
-  overflow: auto;
-  .app-quienes-somos__background {
-    width: 100%;
-    background: #0f0f0f;
-    position: fixed;
-    background-size: cover;
-    background-position: center;
-    top: 80px;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-  }
-  .app-quienes-somos__container {
-    position: relative;
-    z-index: 3;
-    margin: auto;
-    padding: 0;
-    margin-top: 5%;
-    width: 70%;
-    color: $white;
-    user-select: none;
-    font-size: 1.2rem;
-    border-radius: 8px;
-    overflow: auto;
-  }
+.video--voces {
+  height: calc(100vh - 80px);
 }
 </style>
