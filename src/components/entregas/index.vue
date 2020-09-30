72f3ad701958bb420652a330f6c8093483e7e4cd
adrian <adry.dabp@gmail.com>
2020-09-30 10:56:54 -0400

    imagenes

--- src/components/entregas/index.vue
+++ src/components/entregas/index.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <stack
-      :column-min-width="300"
-      :gutter-width="15"
-      :gutter-height="15"
-      monitor-images-loaded
-      >
-      <stack-item
-        v-for="(image, i) in aImages"
+     <v-carousel
+      height="100%"
+      hide-delimiter-background
+      show-arrows-on-hover
+      hide-delimiters
+     >
+      <v-carousel-item
+        v-for="(item,i) in aImages"
         :key="i"
-        style="transition: transform 300ms"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
       >
-        <img :src="image.href" :alt="'imagen'" />
-    </stack-item>
-  </stack>
+        <lightbox :items="item.items" :cells="5" class="entregas"></lightbox>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script>
-import { Stack, StackItem } from "vue-stack-grid";
+import lightbox from '../../plugins/@morioh/v-lightbox/src/lightbox';
+import '../../plugins/@morioh/v-lightbox/dist/lightbox.css';
 export default {
   data() {
     return {
@@ -29,24 +31,34 @@ export default {
     };
   },
   components: {
-    Stack, 
-    StackItem
+    lightbox
   },
   mounted () {
-    this.aImages = [
-      {title: 'Entrega número 1', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.12.33.png'},
-      {title: 'Entrega número 2', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.13.41.png'},
-      {title: 'Entrega número 3', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.15.51.png'},
-      {title: 'Entrega número 4', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.33.53.png'},
-      {title: 'Entrega número 5', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.35.02.png'},
-      {title: 'Entrega número 6', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.37.04.png'},
-      {title: 'Entrega número 7', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.57.37.png'},
-      {title: 'Entrega número 8', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.58.54.png'},
-      {title: 'Entrega número 9', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.59.48.png'},
-      {title: 'Entrega número 10', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-20%20a%20las%200.06.16.png'},
-      {title: 'Entrega número 11', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-20%20a%20las%200.13.59.png'},
-      {title: 'Entrega número 12', href: 'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-20%20a%20las%2017.06.23.png'}
-    ];
+    this.aImages = [{
+      items: [
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.12.33.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.13.41.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.15.51.png',
+      ]
+    }, {
+      items: [
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.33.53.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.35.02.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.37.04.png',
+      ]
+    }, {
+      items: [
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.57.37.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.58.54.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-19%20a%20las%2023.59.48.png',
+      ]
+    }, {
+      items: [
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-20%20a%20las%200.06.16.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-20%20a%20las%200.13.59.png',
+        'http://www.vocesfortaleza.com/assets/entregas/Captura%20de%20pantalla%202020-06-20%20a%20las%2017.06.23.png'
+      ]
+    }];
   }
 };
 </script>
@@ -54,7 +66,7 @@ export default {
 <style lang="scss">
 @import "../../scss/variables.scss";
 
-.entregas {
+.entregas > div {
   margin: 0;
   padding: 0;
   width: 100%;
